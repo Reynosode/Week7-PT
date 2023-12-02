9f972e7d78fa4e425cb869fc7ed1f362b00ae350
Reynosode <Reynoso.David24@gmail.com>
2023-12-01 20:42:04 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// Question 1.
+
+const ages = [3,9,23,64,2,8,28,93];
+
+let newValue = (ages[0] - (ages.length - 1));
+    console.log(newValue);
+
+
+// Question 2.
+
+
+
+// Question 3.
+
+
+
+// Question 4.
+
+
+
+// Question 5. 
+
+
+
+// Question 6.
+
+
+
+// Question 7.
+
+
+
+// Question 8.
+
+
+
+// Question 9.
+
+
+
+// Question 10.
+
+
+
+// Question 11.
+
+
+
+// Question 12.
